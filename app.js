b30ac967869bcea554c8ca218191a0192a6aa84d
tbarth17 <tommyhbrc@gmail.com>
2017-02-23 13:47:16 -0800

    adds cors package, adds basic routes

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-var express = require('express')
-var app = express()
+(function () {
+    'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    const express = require('express');
+    const app = express();
+    const cors = require('cors');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    app.use(cors());
+
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+    // Public route
+    app.get('/api/data/public', (req, res)=>{
+        let deals = [
+            'public',
+            'stuff',
+            'here'
+        ];
+        res.json(deals);
+    });
+
+    // Private route
+    app.get('/api/user/private', (req,res)=>{
+        let deals = [
+            'private',
+            'stuff',
+            'here'
+        ];
+        res.json(deals);
+    });
+
+
+    app.listen(3000);
+
+    console.log('Example app listening on port 3000!');
+}());
